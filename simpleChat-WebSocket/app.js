a31db79ee1d5cdf1d2a4e0253674c3f690719a82
JuanGonzalez0 <gonzalez.juanet36@gmail.com>
2024-04-26 23:19:29 -0300

    sockets y webSockets

---
+++ simpleChat-WebSocket/app.js
@@ -0,0 +1,27 @@
+//importo webSocket y creo el server
+const webSocket = require('ws')
+const server = new webSocket.Server({port:'8080'})
+
+const welcomeMessage = "¡Bienvenido al chat!";
+
+//server prendido y esperando que un cliente se conecte
+server.on('connection', socket => {
+
+    console.log('Client connected')
+    socket.send(welcomeMessage);
+
+    server.clients.forEach(client => {
+        if (client !== socket && client.readyState === webSocket.OPEN) {
+            client.send("¡Un nuevo usuario se ha unido al chat!");
+        }
+    });
+
+    socket.on('message', message => { //recibe el mensaje del cliente
+        server.clients.forEach(client=>{ //revisa si el cliente sigue conectado
+            if(client.readyState === webSocket.OPEN)
+                client.send(`${message}`)
+        })
+    })
+})
+
+console.log('socket initialized on port 8080')
